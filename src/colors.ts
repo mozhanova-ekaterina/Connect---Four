f662d833d482872805376ee04e70630b5a311314
Ekaterina Mozhanova <pandafomka@gmail.com>
2024-09-07 21:04:52 +0300

    saves

--- src/colors.ts
+++ src/colors.ts
@@ -1,14 +1,14 @@
-export const colors = [
-  'red',
-  'brown',
-  'black',
-  'pink',
-  'green'
-]
 // export const colors = [
-//   '#fc2646',
-//   '#270a1f',
-//   '#2c75ff',
-//   '#f664af',
-//   '#ffb433'
+//   'red',
+//   'brown',
+//   'black',
+//   'pink',
+//   'green'
 // ]
+export const colors = [
+  '#fc2646',
+  '#270a1f',
+  '#2c75ff',
+  '#f664af',
+  '#ffb433'
+]
