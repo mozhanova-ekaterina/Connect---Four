0814e508a024b34d146491709585f65cd0793f24
Ekaterina Mozhanova <pandafomka@gmail.com>
2024-09-08 11:08:56 +0300

    saves

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { Game } from "./Game";
 
-const game = new Game( 7, 8)
+const game = new Game(7, 8)
 const restart = document.querySelector('.restart') as HTMLElement
+const difficult = document.getElementById('difficult')
 restart.addEventListener('click', () => game.restart())
+difficult?.addEventListener('click', (e) => game.changeDifficult(e.currentTarget as HTMLSelectElement))
 game.start()
